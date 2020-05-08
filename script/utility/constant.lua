d60268059f1fc13bd9cabb46c3fc3e6d8772b07b
gaven.yang <gaven.yang@dsky.com>
2020-05-08 13:22:41 +0800

    定义核心系统常量

--- script/utility/constant.lua
+++ script/utility/constant.lua
@@ -1,13 +1,17 @@
 --constant.lua
 
+--核心基础错误
 local KernCode = enum("KernCode", 0)
 
-KernCode.SUCCESS      = 0      --成功
-KernCode.LOGIC_FAILED = 1      --业务执行失败
-KernCode.MYSQL_FAILED = 2      --MYSQL执行失败
-KernCode.MONGO_FAILED = 3      --MONGO执行失败
-KernCode.PARAM_ERROR  = 5      --业务参数错误
-KernCode.RPC_FAILED   = 6      --RPC调用失败
+KernCode.SUCCESS            = 0     --成功
+KernCode.LOGIC_FAILED       = 1     --业务执行失败
+KernCode.MYSQL_FAILED       = 2     --MYSQL执行失败
+KernCode.MONGO_FAILED       = 3     --MONGO执行失败
+KernCode.NETWORK_ERROR      = 4     --网络错误
+KernCode.PARAM_ERROR        = 5     --业务参数错误
+KernCode.RPC_FAILED         = 6     --RPC调用失败
+PlatCode.OPERATOR_SELF      = 7     --不能对自己操作
+PlatCode.PLAYER_NOT_EXIST   = 8     --不能对自己操作
 
 --rpc 类型定义
 local RpcType = enum("RpcType", 0, "RPC_REQ", "RPC_RES")
@@ -19,5 +23,4 @@ NetwkTime.RPC_CALL_TIMEOUT  = 5000      --RPC调用超时时间
 NetwkTime.ROUTER_TIMEOUT    = 10000     --router连接超时时间
 NetwkTime.NETWORK_TIMEOUT   = 35000     --其他网络连接超时时间
 NetwkTime.RECONNECT_TIME    = 5         --RPC连接重连时间
-NetwkTime.HEARTBEAT_TIME    = 2000      --RPC连接心跳时间
-NetwkTime.HEARTBEAT_TIME    = 2000      --RPC连接心跳时间
+NetwkTime.HEARTBEAT_TIME    = 1000      --RPC连接心跳时间
