18a0090ae5ef929f27541c2bb67e1dbad2172eff
蔡亮 <enic.cai@idreamsky.com>
2020-05-15 10:10:20 +0000

    添加新的string扩展函数

--- script/basic/string.lua
+++ script/basic/string.lua
@@ -8,6 +8,7 @@ local sfind     = string.find
 local supper    = string.upper
 local slower    = string.lower
 local sformat   = string.format
+local sbyte     = string.byte
 
 --------------------------------------------------------------------------------
 function string_ext.parse(str)
@@ -49,4 +50,37 @@ end
 
 function string_ext.ends_with(str, ending)
     return str:sub(-#ending) == ending
+end
+
+function string_ext.split_to_chars(src)
+    local num = 0
+    local results = {}
+    if not src or type(src) ~= "string" or #src <= 0 then
+        return results
+    end
+    local i = 1
+    local src_bytes = #src
+	while true do
+        local curByte = sbyte(src, i)
+        local byteCount
+        -- [0x00, 0x7f] [0x80, 0x7ff] [0x800, 0xd7ff] [0x10000, 0x10ffff]
+        if curByte > 239 then
+            byteCount = 4  -- 4字节字符
+        elseif curByte > 223 then
+            byteCount = 3  -- 汉字
+        elseif curByte > 128 then
+            byteCount = 2  -- 双字节字符
+        else
+            byteCount = 1  -- 单字节字符
+        end
+        local char = ssub(src, i, i + byteCount - 1)
+        num = num + 1
+        results[num] = char
+        i = i + byteCount
+        if i > src_bytes then
+            break
+        end
+    end
+
+    return results
 end
