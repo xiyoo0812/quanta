bea787052dc279ae82fae39ee54483f5d9cc1186
xiyoo0812 <xiyoo0812@163.com>
2021-01-06 21:09:26 +0800

    数据库代码优化重构

--- script/kernel/store/db_agent.lua
+++ script/kernel/store/db_agent.lua
@@ -9,39 +9,39 @@ local DBGROUP_AREA      = DBGroup.AREA
 
 local DBAgent = singleton()
 function DBAgent:__init()
-    self.part_id = env_number("QUANTA_PART_ID")
+    self.area_id = env_number("QUANTA_PART_ID")
 end
 
 --发送数据库请求
 --{coll_name, selector, fields}
-function DBAgent:find_one(hash_key, db_query, db_group, db_index)
-    return router_mgr:call_dbsvr_hash(hash_key, "find_one", db_group or DBGROUP_AREA, db_index or self.part_id, tunpack(db_query))
+function DBAgent:find_one(hash_key, db_query, db_group, db_area)
+    return router_mgr:call_dbsvr_hash(hash_key, "find_one", db_group or DBGROUP_AREA, db_area or self.area_id, tunpack(db_query))
 end
 
 --发送数据库请求
 --{coll_name, selector, fields, sorts, limit}
-function DBAgent:find(hash_key, db_query, db_group, db_index)
-    return router_mgr:call_dbsvr_hash(hash_key, "find", db_group or DBGROUP_AREA, db_index or self.part_id, tunpack(db_query))
+function DBAgent:find(hash_key, db_query, db_group, db_area)
+    return router_mgr:call_dbsvr_hash(hash_key, "find", db_group or DBGROUP_AREA, db_area or self.area_id, tunpack(db_query))
 end
 
 --{coll_name, selector, single}
-function DBAgent:delete(hash_key, db_query, db_group, db_index)
-    return router_mgr:call_dbsvr_hash(hash_key, "delete", db_group or DBGROUP_AREA, db_index or self.part_id, tunpack(db_query))
+function DBAgent:delete(hash_key, db_query, db_group, db_area)
+    return router_mgr:call_dbsvr_hash(hash_key, "delete", db_group or DBGROUP_AREA, db_area or self.area_id, tunpack(db_query))
 end
 
 --{coll_name, obj, selector, upsert, multi}
-function DBAgent:update(hash_key, db_query, db_group, db_index)
-    return router_mgr:call_dbsvr_hash(hash_key, "update", db_group or DBGROUP_AREA, db_index or self.part_id, tunpack(db_query))
+function DBAgent:update(hash_key, db_query, db_group, db_area)
+    return router_mgr:call_dbsvr_hash(hash_key, "update", db_group or DBGROUP_AREA, db_area or self.area_id, tunpack(db_query))
 end
 
 --{coll_name, obj}
-function DBAgent:insert(hash_key, db_query, db_group, db_index)
-    return router_mgr:call_dbsvr_hash(hash_key, "insert", db_group or DBGROUP_AREA, db_index or self.part_id, tunpack(db_query))
+function DBAgent:insert(hash_key, db_query, db_group, db_area)
+    return router_mgr:call_dbsvr_hash(hash_key, "insert", db_group or DBGROUP_AREA, db_area or self.area_id, tunpack(db_query))
 end
 
 --{coll_name, selector}
-function DBAgent:count(hash_key, db_query, db_group, db_index)
-    return router_mgr:call_dbsvr_hash(hash_key, "count", db_group or DBGROUP_AREA, db_index or self.part_id, tunpack(db_query))
+function DBAgent:count(hash_key, db_query, db_group, db_area)
+    return router_mgr:call_dbsvr_hash(hash_key, "count", db_group or DBGROUP_AREA, db_area or self.area_id, tunpack(db_query))
 end
 
 ------------------------------------------------------------------
