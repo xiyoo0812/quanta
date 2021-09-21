c52cfd133aa51d89a8fd82cdf70f0edf657b8739
xiyoo0812 <xiyoo0812@163.com>
2021-09-22 01:47:28 +0800

    sol改写日志模块

--- script/basic/logger.lua
+++ script/basic/logger.lua
@@ -9,32 +9,17 @@ local sformat       = string.format
 local dgetinfo      = debug.getinfo
 local tpack         = table.pack
 local tunpack       = table.unpack
-local linfo         = llog.info
-local lwarn         = llog.warn
-local ldump         = llog.dump
-local ldebug        = llog.debug
-local lerror        = llog.error
-local lfatal        = llog.fatal
 local lfilter       = llog.filter
 local is_filter     = llog.is_filter
 local lserialize    = lbuffer.serialize
-
-local LOG_LEVEL_DEBUG   = 1
-local LOG_LEVEL_INFO    = 2
-local LOG_LEVEL_WARN    = 3
-local LOG_LEVEL_DUMP    = 4
-local LOG_LEVEL_ERROR   = 5
-local LOG_LEVEL_FATAL   = 6
---local LOG_LEVEL_OFF     = 100
 
 logger = {}
 function logger.init(max_line)
     local log_name  = sformat("%s-%d", quanta.service, quanta.index)
     local log_path = sformat("%s/%s/", environ.get("QUANTA_LOG_PATH"), quanta.service)
-    local log_daemon = environ.status("QUANTA_DAEMON")
-    llog.init(log_path, log_name, 0, max_line or 100000, log_daemon)
+    llog.init(log_path, log_name, 0, max_line or 100000)
     logger.filter(environ.number("QUANTA_LOG_LVL"))
-    if log_daemon then
+    if environ.status("QUANTA_DAEMON") then
         quanta.daemon()
     end
 end
@@ -42,7 +27,7 @@ end
 logger.close = llog.close
 
 function logger.filter(level)
-    for lvl = LOG_LEVEL_DEBUG, LOG_LEVEL_FATAL do
+    for lvl = LOG_LEVEL.DEBUG, LOG_LEVEL.FATAL do
         --llog.filter(level, on/off)
         lfilter(lvl, lvl >= level)
     end
@@ -62,12 +47,12 @@ local function logger_output(method, fmt, extend, swline, ...)
 end
 
 local LOG_LEVEL_METHOD = {
-    [LOG_LEVEL_INFO]    = { "info",  linfo,  false, false  },
-    [LOG_LEVEL_WARN]    = { "warn",  lwarn,  true,  false  },
-    [LOG_LEVEL_DUMP]    = { "dump",  ldump,  true,  true },
-    [LOG_LEVEL_DEBUG]   = { "debug", ldebug, true,  false },
-    [LOG_LEVEL_ERROR]   = { "err",   lerror, true,  false  },
-    [LOG_LEVEL_FATAL]   = { "fatal", lfatal, true,  false  }
+    [LOG_LEVEL.INFO]    = { "info",  llog.info,  false, false   },
+    [LOG_LEVEL.WARN]    = { "warn",  llog.warn,  true,  false   },
+    [LOG_LEVEL.DUMP]    = { "dump",  llog.dump,  true,  true    },
+    [LOG_LEVEL.DEBUG]   = { "debug", llog.debug, true,  false   },
+    [LOG_LEVEL.ERROR]   = { "err",   llog.error, true,  false   },
+    [LOG_LEVEL.FATAL]   = { "fatal", llog.fatal, true,  false   }
 }
 for lvl, conf in pairs(LOG_LEVEL_METHOD) do
     local name, method, extend, swline = tunpack(conf)
