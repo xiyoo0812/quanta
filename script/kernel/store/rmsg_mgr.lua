97804aaa6b753029ecce02c97555256d4e6111f0
xiyoo0812 <xiyoo0812@163.com>
2021-09-28 12:40:29 +0800

    rmsg支持ttl

--- script/kernel/store/rmsg_mgr.lua
+++ script/kernel/store/rmsg_mgr.lua
@@ -4,6 +4,7 @@ local lcrypt = require("lcrypt")
 local bson          = require("bson")
 
 local otime         = os.time
+local bdate         = bson.date
 local tsort         = table.sort
 local log_err       = logger.err
 local log_info      = logger.info
@@ -16,16 +17,22 @@ local DBGroup       = enum("DBGroup")
 local DBGROUP_HASH  = DBGroup.HASH
 
 local RmsgMgr = class()
+local prop = property(RmsgMgr)
+prop:reader("table_name", "") -- table_name
+prop:reader("ttl", nil)       -- ttl
 
-function RmsgMgr:__init(name)
-    self.db_table_name = name
+function RmsgMgr:__init(name, ttl)
+    if ttl then
+        self.ttl = ttl
+        self:build_ttl(name)
+    end
+    self.table_name = name
     log_info("[RmsgMgr][init] init rmsg table: %s", name)
-    self:build_ttl()
 end
 
-function RmsgMgr:build_ttl()
-    log_info("[RmsgMgr][build_ttl] rmsg table:%s",self.db_table_name)
-    local query = {self.db_table_name, { { key = { due_time = 1 }, expireAfterSeconds = 0, name = "due_time", unique = false } }}
+function RmsgMgr:build_ttl(name)
+    log_info("[RmsgMgr][build_ttl] rmsg table:%s", name)
+    local query = { name, { { key = { ttl = 1 }, expireAfterSeconds = 0, name = "ttl", unique = false } }}
     local ok, code, result = db_agent:build_indexes(1, query, DBGROUP_HASH)
     if ok and check_success(code) then
         log_info("[RmsgMgr][build_ttl] rmsg table %s build due index success")
@@ -34,7 +41,7 @@ end
 
 -- 查询未处理消息列表
 function RmsgMgr:list_message(to)
-    local query = {self.db_table_name, {to = to, deal_time = 0}, {_id = 0}}
+    local query = {self.table_name, {to = to, deal_time = 0}, {_id = 0}}
     local ok, code, result = db_agent:find(to, query, DBGROUP_HASH, to)
     if ok and check_success(code) then
         tsort(result, function(a, b)
@@ -47,18 +54,18 @@ end
 -- 设置信息为已处理
 function RmsgMgr:deal_message(to, uuid)
     log_info("[RmsgMgr][deal_message] deal message: %s", uuid)
-    local query = {self.db_table_name, {["$set"] = {deal_time = quanta.now}}, {uuid = uuid}}
+    local query = {self.table_name, {["$set"] = {deal_time = quanta.now}}, {uuid = uuid}}
     return db_agent:update(to, query, DBGROUP_HASH, to)
 end
 
 -- 删除消息
 function RmsgMgr:delete_message(to, uuid)
     log_info("[RmsgMgr][delete_message] delete message: %s", uuid)
-    return db_agent:delete(to, {self.db_table_name, {uuid = uuid}}, DBGROUP_HASH, to)
+    return db_agent:delete(to, {self.table_name, {uuid = uuid}}, DBGROUP_HASH, to)
 end
 
 -- 发送消息
-function RmsgMgr:send_message(from, to, typ, body, id,due_second)
+function RmsgMgr:send_message(from, to, typ, body, id)
     local uuid = id or new_guid()
     local doc = {
         uuid = uuid,
@@ -68,11 +75,10 @@ function RmsgMgr:send_message(from, to, typ, body, id,due_second)
         deal_time = 0,
     }
     --设置过期ttl字段
-    if due_second and due_second > 0 then
-        doc.due_time = bson.date(otime + due_second)
+    if self.ttl then
+        doc.ttl = bdate(quanta.now + self.ttl)
     end
-
-    local ok = db_agent:insert(to, {self.db_table_name, doc}, DBGROUP_HASH, to)
+    local ok = db_agent:insert(to, {self.table_name, doc}, DBGROUP_HASH, to)
     if not ok then
         log_err("[RmsgMgr][send_message] send message failed: %s, %s, %s, %s", uuid, from, to, typ)
     else
