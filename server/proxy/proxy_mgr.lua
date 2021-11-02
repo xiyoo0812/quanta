74d1fdcfcee0d3cc563df45a6b39ef76e9b0a926
xiyoo0812 <xiyoo0812@163.com>
2021-11-02 20:49:23 +0800

    完善ProxyMgr

--- server/proxy/proxy_mgr.lua
+++ server/proxy/proxy_mgr.lua
@@ -13,8 +13,8 @@ function ProxyMgr:__init()
     event_mgr:add_listener(self, "rpc_http_get")
 end
 
-function ProxyMgr:rpc_http_get(url, query, headers)
-    local ok, status, res = http_client:call_get(url, query, headers)
+function ProxyMgr:rpc_http_get(url, querys, headers)
+    local ok, status, res = http_client:call_get(url, querys, headers)
     if not ok then
         log_err("ProxyMgr:rpc_http_get ok=%s, status=%s", ok, status)
         return 404
@@ -22,10 +22,28 @@ function ProxyMgr:rpc_http_get(url, query, headers)
     return status, res
 end
 
-function ProxyMgr:rpc_http_post(url, post_data, headers)
-    local ok, status, res = http_client:call_post(url, post_data, headers)
+function ProxyMgr:rpc_http_post(url, post_data, headers, querys)
+    local ok, status, res = http_client:call_post(url, post_data, headers, querys)
     if not ok then
         log_err("ProxyMgr:rpc_http_post ok=%s, status=%s", ok, status)
+        return 404
+    end
+    return status, res
+end
+
+function ProxyMgr:rpc_http_put(url, put_data, headers, querys)
+    local ok, status, res = http_client:call_put(url, put_data, headers, querys)
+    if not ok then
+        log_err("ProxyMgr:rpc_http_put ok=%s, status=%s", ok, status)
+        return 404
+    end
+    return status, res
+end
+
+function ProxyMgr:rpc_http_del(url, querys, headers)
+    local ok, status, res = http_client:call_del(url, querys, headers)
+    if not ok then
+        log_err("ProxyMgr:rpc_http_del ok=%s, status=%s", ok, status)
         return 404
     end
     return status, res
