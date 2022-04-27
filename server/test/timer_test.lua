0e202ef5a3e9077a112f5b210a546a834d6cf385
xiyoo0812 <xiyoo0812@163.com>
2022-04-28 00:47:10 +0800

    修复lambda函数捕获导致BUG

--- server/test/timer_test.lua
+++ server/test/timer_test.lua
@@ -15,20 +15,28 @@ local steady_ms = ltimer.steady_ms()
 log_info("time: steady:%s, steady_ms:%s", steady, steady_ms)
 
 local timer_mgr = quanta.get("timer_mgr")
+local thread_mgr = quanta.get("thread_mgr")
 
-log_info("once")
-timer_mgr:once(500, function(escape_ms)
-    log_info("once: %s", escape_ms)
-end)
+thread_mgr:fork(function()
+    log_info("once")
+    timer_mgr:once(500, function(escape_ms)
+        log_info("once: %s", escape_ms)
+    end, 3)
 
-log_info("loop")
-timer_mgr:loop(1000, function(escape_ms)
-    log_info("loop: %s", escape_ms)
-end)
+    log_info("loop")
+    timer_mgr:loop(1000, function(escape_ms)
+        log_info("loop: %s", escape_ms)
+    end)
 
-log_info("register")
-timer_mgr:register(500, 1000, 5, function(escape_ms)
-    log_info("register: %s", escape_ms)
+    log_info("register")
+    timer_mgr:register(500, 1000, 5, function(escape_ms)
+        log_info("register: %s", escape_ms)
+    end)
+
+    local function load_strongest_players()
+        return false, {}
+    end
+
 end)
 
 --os.exit()
