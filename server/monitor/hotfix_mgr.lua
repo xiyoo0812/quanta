9223ad521d5ef5097a795b6046cdbf79b51729a9
xiyoo0812 <xiyoo0812@163.com>
2023-01-18 10:04:41 +0000

    热更新流程完善

--- server/monitor/hotfix_mgr.lua
+++ server/monitor/hotfix_mgr.lua
@@ -1,5 +1,4 @@
 --monitor_mgr.lua
-local lcodec        = require("lcodec")
 local lstdfs        = require("lstdfs")
 
 local iopen         = io.open
@@ -13,7 +12,6 @@ local lremove       = lstdfs.remove
 local ltemp_dir     = lstdfs.temp_dir
 local lpardir       = lstdfs.parent_path
 local lcurdir       = lstdfs.current_path
-local serialize     = lcodec.serialize
 
 local nacos         = quanta.get("nacos")
 local thread_mgr    = quanta.get("thread_mgr")
@@ -59,13 +57,15 @@ function HotfixMgr:encrypt(path, content)
     local file_path = lpardir(temp_name)
     lmkdir(file_path)
     -- 写入热更文件
-    local temp_file<close> = iopen(temp_path, "w")
+    local temp_file = iopen(temp_name, "w")
     temp_file:write(content)
+    temp_file:flush()
+    temp_file:close()
     -- 加密热更文件
     local parent_path = lpardir(lcurdir())
     local luac = lappend(lcurdir(), "luac")
     local output_path = lappend(parent_path, path)
-    oexec(sformat("%s -o %s %s", luac, output_path, temp_path))
+    oexec(sformat("%s -o %s %s", luac, output_path, temp_name))
     --删除临时文件
     lremove(temp_path, true)
 end
@@ -74,33 +74,33 @@ end
 function HotfixMgr:update_config(data_id, group, path)
     local content = nacos:get_config(data_id, group);
     if not content then
-        log_warn("[HotfixMgr][update_config] update data_id:%s failed!", data_id)
+        log_warn("[HotfixMgr][update_config] update script:%s failed!", path)
         return
     end
     self:encrypt(path, content)
+    log_debug("[HotfixMgr][update_config] update script: %s success!", path)
 end
 
 --热更
 function HotfixMgr:hotfix_callback(group, md5, cvalue)
-    self.config.md5 = md5
-    local parent_path = lpardir(lcurdir())
-    --保存热更配置
-    local version_name = lappend(parent_path, "server/hotfix/version.lua")
-    local fversion<close> = iopen(version_name, "w")
-    local content = sformat("-- version.lua\n\nreturn %s", serialize(self.config, true))
-    fversion:write(content)
-    --保存文件字典
-    local dict_name = lappend(parent_path, "server/hotfix/dictionary.lua")
-    local fdict<close> = iopen(dict_name, "w")
-    fdict:write(cvalue)
-    --加载文件字典
-    local dictionary = import("hotfix/dictionary.lua")
+    --加载字典文件
+    local dic_func, err = load(cvalue)
+    if not dic_func then
+        log_warn("[HotfixMgr][hotfix_callback] load dictionary failed: %s !", err)
+        return
+    end
+    --更新版本配置
+    self:update_config("server-hotfix-version.lua", group, "server/hotfix/version.lua")
+    self:update_config("server-hotfix-dictionary.lua", group, "server/hotfix/dictionary.lua")
+    --比对更新文件
+    local dictionary = dic_func()
     for data_id, info in pairs(dictionary) do
         local old_info = self.dictionary[data_id]
         if not old_info or info.md5 ~= old_info.md5 then
             self:update_config(data_id, group, info.path)
         end
     end
+    self.config.md5 = md5
     self.dictionary = dictionary
     --通知其他服务热更新
     monitor_mgr:broadcast_all("rpc_service_hotfix")
