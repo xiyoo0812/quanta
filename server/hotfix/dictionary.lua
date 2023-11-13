9198243ff0fba87b3b27e4e05270d84764732ee1
wp20221109 <wangpeng@pathea.net>
2023-11-13 10:02:08 +0000

    废弃nacos热更方案

--- server/hotfix/dictionary.lua
+++
@@ -1,8 +0,0 @@
--- dictionary.lua
-
-return {
-    ['quanta-script-agent-gm_agent.lua'] = {
-        md5 = 'bcb9ab5f517c6b6b47d34784403538e12',
-        path = 'quanta/script/agent/gm_agent.lua'
-    }
-}
--- server/monitor.lua
+++ server/monitor.lua
@@ -4,5 +4,4 @@ import("kernel.lua")
 quanta.startup(function()
     --初始化monitor
     import("monitor/monitor_mgr.lua")
-    import("monitor/hotfix_mgr.lua")
 end)
